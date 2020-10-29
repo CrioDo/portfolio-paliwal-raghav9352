b1819746161432fe603143e54fe5b21ecdce6a34
CrioDo <rahul@criodo.com>
2020-10-29 20:40:45 +0530

    Updates

---
+++ precache-manifest.6c4298a01f05076109ffc76f07faa017.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f966bb8e22ffabb84276932c8a61ca1",
+    "url": "/portfolio-paliwal-raghav9352/index.html"
+  },
+  {
+    "revision": "fde1be57508daa83510c",
+    "url": "/portfolio-paliwal-raghav9352/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "840d05f8fc53292b6dc1",
+    "url": "/portfolio-paliwal-raghav9352/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "fde1be57508daa83510c",
+    "url": "/portfolio-paliwal-raghav9352/static/js/2.0303cad2.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/portfolio-paliwal-raghav9352/static/js/2.0303cad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840d05f8fc53292b6dc1",
+    "url": "/portfolio-paliwal-raghav9352/static/js/main.670861bc.chunk.js"
+  },
+  {
+    "revision": "be5e2f3767fc2c0aa047",
+    "url": "/portfolio-paliwal-raghav9352/static/js/runtime-main.66363a7e.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/portfolio-paliwal-raghav9352/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/portfolio-paliwal-raghav9352/static/media/crio-logo.63be71d0.svg"
+  }
+]);
